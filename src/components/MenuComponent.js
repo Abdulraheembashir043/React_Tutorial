54629fb8cf70387ab00ee25f5def3235a6445602
Abdulraheem Bashir <abdulraheembashir043@gmail.com>
2021-02-15 16:00:47 +0100

    react components 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, CardBody, CardText } from 'reactstrap';
 
 
 class Menu extends Component {
@@ -7,61 +7,43 @@ class Menu extends Component {
     super(props);
 
     this.state = {
-      dishes: [
-        {
-          id: 0,
-          name: 'Uthappizza',
-          image: 'assets/images/uthappizza.png',
-          categoty: 'mains',
-          label: 'Hot',
-          price: '4.99',
-          description: 'A unique combination of dishes. A unique combination of dishes. A unique combination of dishes. A unique combination of dishes.'
-        },
-        {
-          id: 1,
-          name: 'Zucchipakoda',
-          image: 'assets/images/zucchipakoda.png',
-          categoty: 'appetizer',
-          label: '',
-          price: '1.99',
-          description: 'Deeep fried Dish. Deeep fried Dish. Deeep fried Dish. Deeep fried Dish. Deeep fried Dish. Deeep fried Dish.'
-        },
-        {
-          id: 2,
-          name: 'Vadonut',
-          image: 'assets/images/vadonut.png',
-          categoty: 'appetizer',
-          label: 'New',
-          price: '1.99',
-          description: 'A quintessential Confusion Dishes. A quintessential Confusion Dishes. A quintessential Confusion Dishes. A quintessential Confusion Dishes. A quintessential Confusion Dishes.'
-        },
-        {
-          id: 3,
-          name: 'Elaicheese Cake',
-          image: 'assets/images/elaicheesecake.png',
-          categoty: 'dessert',
-          label: '',
-          price: '2.99',
-          description: 'A delictable, Semisweet Dishes. A delictable, Semisweet Dishes. A delictable, Semisweet Dishes. A delictable, Semisweet Dishes. A delictable, Semisweet Dishes. A delictable, Semisweet Dishes.'
-        }
-      ]
+      selectedDish: null
+    }
+  }
+
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish })
+  }
+
+  renderDish(dish) {
+    if(dish != null) {
+      return (
+        <Card>
+        <CardImg width='100%' src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+      )
+    } else {
+      return (
+        <div></div>
+      )
     }
   }
 
   render() {
 
-    const menu = this.state.dishes.map((dish) => {
+    const menu = this.props.dishes.map((dish) => {
       return (
-        <div key={dish.id} className='col-12 mt-5'>
-          <Media tag='li'>
-            <Media left middle>
-              <Media object src={dish.image} alt={dish.name} />
-            </Media>
-            <Media body className='ml-5'>
-              <Media heading>{dish.name}</Media>
-              <p>{dish.description}</p>
-            </Media>
-          </Media>
+        <div key={dish.id} className='col-12 col-md-5 m-1'>
+          <Card onClick = {() => this.onDishSelect(dish)} >
+              <CardImg width='100%' src={dish.image} alt={dish.name} />
+              <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+              </CardImgOverlay>
+          </Card>
         </div>
       )
     });
@@ -69,9 +51,10 @@ class Menu extends Component {
     return (
       <div className='contaner'>
         <div className='row'>
-          <Media list>
             { menu }
-          </Media>
+        </div>
+        <div className='row'>
+          {this.renderDish(this.state.selectedDish)}
         </div>
       </div>
     );
---
+++ src/shared/baseUrl.js
@@ -0,0 +1,2 @@
+//configuration file for client client-server ommunication
+export const baseUrl = 'http://localhost:3001/'; //refers to the base Url for our Server
